1503799317ee379756c2bf4a561a47a4b2a4fe62
Mcalvin7020 <mcalvin7020@sohamcollege.org.uk>
2014-06-04 19:56:18 +0100

    Update tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,7 @@
-#paste code here
+answer="yes"
+while answer !="no":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    total=num1*num2
+    print("{0} and {1} multiplied make {2}".format(num1,num2,total))
+    answer=input("again? ")
