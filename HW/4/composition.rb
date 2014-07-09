0e2e9a403f53a72443f3f63ba196f8317f567c6b
Mcalvin7020 <morgancalvin1999@gmail.com>
2014-07-09 13:53:06 +0100

    Update composition.rb

--- HW/4/composition.rb
+++ HW/4/composition.rb
@@ -1,1 +1,78 @@
 #paste code here
+play 45
+
+sleep 1
+
+play 40
+
+sleep 1
+
+play 49
+
+sleep 0.5
+
+play 40
+
+sleep 0.6
+
+play 50
+
+sleep 0.7
+
+play 35
+
+sleep 0.3
+
+play 40
+
+sleep 0.5
+
+play 49
+
+sleep 0.7
+
+play 45
+
+sleep 0.5
+
+play 40
+
+sleep 0.6
+
+play 30
+
+sleep 0.2
+
+play 33
+
+sleep 0.2
+
+play 36
+
+sleep 0.2
+
+play 39
+
+sleep 0.2
+
+play 42
+
+sleep 0.2
+
+play 45
+
+sleep 0.2
+
+play 48
+
+sleep 0.2
+
+play 51
+
+sleep 0.2
+
+play 45
+
+sleep 0.2
+
+play 40
